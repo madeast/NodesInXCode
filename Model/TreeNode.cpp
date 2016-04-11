7db403ed892bf8f5da689d935ff8a52746c8e7b7
Easton Madsen <eeagle25@comcast.net>
2016-04-11 08:38:02 -0600

    Have the Three TreeNode Methods

--- Model/TreeNode.cpp
+++ Model/TreeNode.cpp
@@ -7,3 +7,34 @@
 //
 
 #include "TreeNode.h"
+
+template <class Type>
+TreeNode<Type> :: TreeNode() : Node<Type>()
+{
+    this->leftChild = nullptr;
+    this->rightChild = nullptr;
+    this->parent = nullptr;
+}
+
+template <class Type>
+TreeNode<Type> :: TreeNode(const Type& value) : Node<Type>(value)
+{
+    this->leftChild = nullptr;
+    this->rightChild = nullptr;
+    this->parent = nullptr;
+    
+    this->setValue(value);
+}
+
+template <class Type>
+TreeNode<Type> :: TreeNode(const Type& value, TreeNode<Type> * parent) : Node<Type>(value)
+{
+    this->leftChild = nullptr;
+    this->rightChild = nullptr;
+    
+    this->setValue(value);
+    this->setParent(parent);
+}
+
+template <class Type>
+TreeNode<
